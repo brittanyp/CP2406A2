f7c965f14130d3b45479b8c1c27ad22783daba02
brittanyp <brittany.peever@my.jcu.edu.au>
2016-09-14 14:33:36 +1000

    Working on reading play cards and their value
    creation of function addNormalDetail and bugtesting soon to be function
    addArrayDetail.

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -25,8 +25,9 @@ public class ReadFile {
         Element deckElement = (Element) fileNode;
         NodeList deckNodes = deckElement.getElementsByTagName("dict");
 
-        //for(int i = 0; i < deckNodes.getLength(); i++){
-            int i = 0;
+        for(int i = 0; i < deckNodes.getLength(); i++){
+            //Todo: Delete this line below, testing
+            //int i = 0;
             Node cardNode = deckNodes.item(i);
             Element cardElement = (Element) cardNode;
 
@@ -38,26 +39,42 @@ public class ReadFile {
             //System.out.println(cardType);
 
             if (cardType.equals("play")) {
-                //Normal Detail
-                Node theValueNode = cardElement.getElementsByTagName("string").item(0);
+                addNormalDetails(cardElement, i, 0, 6);
+                //ArrayDetail start
+                Node theValueNode = cardElement.getElementsByTagName("array").item(0);
                 Element valueElement = (Element) theValueNode;
-                System.out.println(i + "\n" + valueElement.getTextContent());
+                NodeList array = valueElement.getElementsByTagName("string");
 
-                //Array Detail
-                theValueNode = cardElement.getElementsByTagName("array").item(0);
-                valueElement = (Element) theValueNode;
-                //CARRY ON HERE
-                System.out.println(i + "\n" + valueElement.getTextContent());
+                int amntinArray = array.getLength();
+                //Todo: Delete this line below, testing
+                //System.out.println("Amt in array " + amntinArray);
+                for (int x = 0; x < array.getLength(); x++) {
+                    System.out.println(array.item(x).getTextContent());
+                }
 
-
-
+                //ArrayDetail End
+                addNormalDetails(cardElement, i, 7 + amntinArray, 14);
             }
             else{
                 System.out.println("Trump card");
             }
 
 
-        //}
+        }
 
     }
+
+    public static void addNormalDetails(Element cElement, int indexI, int start, int end){
+        Element cardElement = cElement;
+        int indexStart = start;
+        int indexEnd = end;
+        int i = indexI;
+        for (int index = indexStart; index < indexEnd; index++) {
+            //Normal Detail
+            Node theValueNode = cardElement.getElementsByTagName("string").item(index);
+            Element valueElement = (Element) theValueNode;
+            System.out.println(i + "\n" + valueElement.getTextContent());
+        }
+    }
 }
+
